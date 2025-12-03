08f4acb9f9bf4261432a13d408aeaaa78d3edc96
akzvitor <vitor_eterno@outlook.com>
2025-12-02 23:15:12 -0300

    adiciona dependencias e implementa classes de configuracao do executor

---
+++ resilient-executor/src/main/java/com/akzv/resexecutor/resilientExecutor/ExecutorConfig.java
@@ -0,0 +1,13 @@
+package com.akzv.resexecutor.resilientExecutor;
+
+public class ExecutorConfig<T> {
+    public RetryConfig retryConfig;
+    public TimeoutConfig timeoutConfig;
+    public FallbackFunction<T> fallback;
+
+    public ExecutorConfig(RetryConfig retryConfig, TimeoutConfig timeoutConfig, FallbackFunction<T> fallback) {
+        this.retryConfig = retryConfig;
+        this.timeoutConfig = timeoutConfig;
+        this.fallback = fallback;
+    }
+}
